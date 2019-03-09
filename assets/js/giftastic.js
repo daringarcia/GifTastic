82a9c033dc86d74dddb83c40c61a784891258f55
DG El Matador <daringarcia@gmail.com>
2019-03-09 00:47:53 -0500

    resubmit

---
+++ assets/js/giftastic.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+
+    var displayedButtons = ["THE MATRIX", "RED PILL", "BLUE PILL"];
+
+    function displayImg(){
+
+        $("#display-images").empty();
+        var input = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=aMtaHZAX92OXQ4FEySR2R44Vu0hv2iY8";   
+
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        }).done(function(response) {
+
+            for(var i = 0; i < limit; i++) {    
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+
+                // var elem = document.getElementById('holder')
+                // elem.style.width = '250px';
+            
+                var image = $("<img>");
+                image.attr("src", response.data[i].images.original_still.url);
+                image.attr("data-still", response.data[i].images.original_still.url);
+                image.attr("data-animate", response.data[i].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+                var rating = response.data[i].rating;
+                console.log(response);
+                var pRating = $("<p>").text("Rating: " + rating);
+                displayDiv.append(pRating)
+
+                $("#display-images").append(displayDiv);
+            }
+        });
+    }
+
+    function renderButtons(){ 
+
+        $("#display-buttons").empty();
+
+        for (var i = 0; i < displayedButtons.length; i++){
+
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");//
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", displayedButtons[i]); 
+            newButton.text(displayedButtons[i]); 
+            $("#display-buttons").append(newButton); 
+        }
+    }
+
+    function imageChangeState() {          
+
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }   
+    }
+
+    $("#submitPress").on("click", function(e){
+
+        // if($('#user-input').val().length < 1)
+        //     e.preventDefault();
+
+        // else
+        
+        var input = $("#user-input").val().trim();
+        form.reset();
+        displayedButtons.push(input);
+                
+        renderButtons();
+
+        return false;
+    })
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImg);
+    $(document).on("click", ".gif", imageChangeState);
+});
